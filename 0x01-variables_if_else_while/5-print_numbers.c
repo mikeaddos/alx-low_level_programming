1d5918c41465fb25959d77665d8cfde9452ae18b
mikeaddos <mikeaddos@yahoo.com>
2023-02-17 09:33:29 -0800

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all single digit numbers of base 10
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a < 10 ; a++)
+		printf("%d", a);
+	putchar('\n');
+	retun(0);
+}
