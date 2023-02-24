d63b783b527070689806be2995954fbf5ca80d42
mikeaddos <mikeaddos@yahoo.com>
2023-02-23 16:39:39 -0800

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - checks for checks for a digit (0 through).
+ * @m: m - Variable
+ * @n: n - variable
+ * Return: Always 0.
+ */
+int mul(int m, int n)
+{
+	int mul;
+
+	mul = m * n;
+	return (mul);
+}
