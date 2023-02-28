1a4813e511416d0d3b8ebb401875bf3c736b51fd
mikeaddos <mikeaddos@yahoo.com>
2023-02-27 17:58:34 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_puts - prints a string
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void which means our answer is correct
+ */
+
+void _puts(char *str)
+{
+char *a;
+int b;
+
+a = str;
+
+for (b = 0; a[b]; b++)
+{
+_putchar(a[b]);
+}
+_putchar('\n');
+}
