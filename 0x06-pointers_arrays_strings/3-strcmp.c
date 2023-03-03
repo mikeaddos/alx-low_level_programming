048b9f7602c72c538a041b5a8ace3185c620d590
mikeaddos <mikeaddos@yahoo.com>
2023-03-02 17:01:59 -0800

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,20 +2,20 @@
 
 /**
  * _strcmp - Compare string values
- * @b1: Input value
- * @b2: Input value
- * Return: b1[i] - b2[i]
+ * @s1: Input value
+ * @s2: Input value
+ * Return: s1[i] - s2[i]
  */
-int _strcmp(char *b1, char *b2)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
-	while (b1[i] != '\0' && b2[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (b1[i] != b2[i])
+		if (s1[i] != s2[i])
 		{
-			return (b1[i] - b2[i]);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
