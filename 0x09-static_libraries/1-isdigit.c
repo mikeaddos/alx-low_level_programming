efbdfaeb1b74987c419b18ec6ca00230e110c07d
mikeaddos <mikeaddos@yahoo.com>
2023-03-10 06:46:18 -0800

    static library libmy

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @q: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int q)
+{
+	return (q >= '0' && q <= '9');
+}
