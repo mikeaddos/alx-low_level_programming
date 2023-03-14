4e761360e075ed2b1c6c093e9f8c14174d60117b
mikeaddos <mikeaddos@yahoo.com>
2023-03-13 23:51:35 -0700

    function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- *
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
  * Return: pointer of an array of chars
  */
+
 char *str_concat(char *s1, char *s2)
 {
 	char *strout;
-	unsigned int i, j, k, limit;
+	unsigned int m, n, k, limit;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (m = 0; s1[m] != '\0'; m++)
 		;
 
-	for (j = 0; s2[j] != '\0'; j++)
+	for (n = 0; s2[n] != '\0'; n++)
 		;
 
-	strout = malloc(sizeof(char) * (i + j + 1));
+	strout = malloc(sizeof(char) * (m + n + 1));
 
 	if (strout == NULL)
 	{
@@ -32,11 +32,11 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (k = 0; k < i; k++)
+	for (k = 0; k < m; k++)
 		strout[k] = s1[k];
 
-	limit = j;
-	for (j = 0; j <= limit; k++, j++)
+	limit = n;
+	for (n = 0; n <= limit; k++, n++)
 		strout[k] = s2[j];
 
 	return (strout);
